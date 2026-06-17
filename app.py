98f0f19fcd66c3c840cc8e509c7d89004130b65c
MaxBaev <mbaev2012@mail.ru>
2026-06-17 13:10:24 +0300

    Добавлен BASE_URL и try-except при запросах в API-функциях

--- app.py
+++ app.py
@@ -5,45 +5,59 @@ import requests
 from pprint import pprint, pp
 from datetime import datetime, date
 
+BASE_URL = "https://backend.cppktrain.ru"
+
 
 def get_all_trains(
-        from_station_id: int, to_station_id: int, origin_date: date) -> list[UnsortedTrain]:
-    url = "https://backend.cppktrain.ru/train-schedule/date-travel"
+        from_station_id: int, to_station_id: int, origin_date: date) -> list[UnsortedTrain] | None:
+    url = f"{BASE_URL}/train-schedule/date-travel"
     params = {
         "date": origin_date,
         "fromStationId": from_station_id,
         "toStationId": to_station_id
     }
-    r = requests.get(url, params=params)
-    result: list[UnsortedTrain] = []
-    for train_dict in r.json():
-        result.append(UnsortedTrain.model_validate(train_dict))
-    return result
+    try:
+        r = requests.get(url, params=params)
+        result: list[UnsortedTrain] = []
+        for train_dict in r.json():
+            result.append(UnsortedTrain.model_validate(train_dict))
+        return result
+    except Exception as e:
+        print(f"An error occurred while completing get_all_trains function:\n{e}")
+        return None
 
 
 def get_all_free_trains(
-        from_station_id: int, to_station_id: int, origin_date: date) -> list[SortedTrain]:
-    url = "https://backend.cppktrain.ru/api/TrainPricing"
+        from_station_id: int, to_station_id: int, origin_date: date) -> list[SortedTrain] | None:
+    url = f"{BASE_URL}/api/TrainPricing"
     params = {
         "departureDate": origin_date.strftime("%Y-%m-%d"),
         "originCode": from_station_id,
         "destinationCode": to_station_id
     }
-    r = requests.get(url, params=params)
-    result: list[SortedTrain] = []
-    for train_dict in r.json():
-        result.append(SortedTrain.model_validate(train_dict))
-    return result
+    try:
+        r = requests.get(url, params=params)
+        result: list[SortedTrain] = []
+        for train_dict in r.json():
+            result.append(SortedTrain.model_validate(train_dict))
+        return result
+    except Exception as e:
+        print(f"An error occurred while completing get_all_free_trains function:\n{e}")
+        return None
 
 
 def get_all_train_places(
-        train_id: str, from_station_id: str, to_station_id: str, origin_date: date) -> TrainPlacesObject:
-    url = "https://backend.cppktrain.ru/api/CarPricing"
+        train_id: str, from_station_id: str, to_station_id: str, origin_date: date) -> TrainPlacesObject | None:
+    url = f"{BASE_URL}/api/CarPricing"
     params = {
         "departuredate": origin_date,
         "origincode": from_station_id,
         "destinationcode": to_station_id,
         "trainnumber": train_id
     }
-    r = requests.get(url, params=params)
-    return TrainPlacesObject.model_validate(r.json())
+    try:
+        r = requests.get(url, params=params)
+        return TrainPlacesObject.model_validate(r.json())
+    except Exception as e:
+        print(f"An error occurred while completing get_all_train_places function:\n{e}")
+        return None
